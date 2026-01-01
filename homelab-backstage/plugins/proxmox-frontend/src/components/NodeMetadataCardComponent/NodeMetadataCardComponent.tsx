3c1e773567628667ab9e75db6e98bf778e570f9d
William Mossberg <william.mossberg@outlook.com>
2026-01-01 13:47:02 +0100

    Worked on layout in NodePage.tsx

---
+++ homelab-backstage/plugins/proxmox-frontend/src/components/NodeMetadataCardComponent/NodeMetadataCardComponent.tsx
@@ -0,0 +1,66 @@
+import { InfoCard, Progress, ResponseErrorPanel, StructuredMetadataTable } from '@backstage/core-components';
+import {
+    useApi,
+    discoveryApiRef,
+    fetchApiRef
+} from '@backstage/core-plugin-api';
+import useAsync from 'react-use/esm/useAsync';
+
+
+type Props = {
+    nodeId: string;
+};
+
+
+function toUptime(seconds: number): string {
+    const days = Math.floor(seconds / (3600 * 24));
+    const hours = Math.floor((seconds % (3600 * 24)) / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+
+    const uptime = Array<String>();
+
+    if (days > 0) {
+        uptime.push(`${days}d`);
+    }
+    if (hours > 0) {
+        uptime.push(`${hours}h`);
+    }
+    
+    uptime.push(`${minutes}m`);
+
+    return uptime.join(' ');
+}
+
+
+export const NodeMetadataCard = (props: Props) => {
+    const fetchApi = useApi(fetchApiRef);
+    const discoveryApi = useApi(discoveryApiRef);
+
+    const { value, loading, error } = useAsync(async () => {
+        const baseUrl = await discoveryApi.getBaseUrl('proxmox');
+        const response = await fetchApi.fetch(`${baseUrl}/nodes/${props.nodeId}`);
+        console.log(response);
+        return response.json();
+    }, [fetchApi, discoveryApi, props.nodeId]);
+
+    if (loading) {
+        return <Progress />
+    }
+
+    if (error) {
+        return <ResponseErrorPanel error={error}/>
+    }
+
+    const metadata = {
+        'CPU Model': value.cpuinfo.model || 'Unknown',
+        'Kernel': value['current-kernel'].release || 'Unknown',
+        'PVE Version': value.pveversion || 'Unknown',
+        'Uptime': toUptime(value.uptime || 0)
+    };
+ 
+    return (
+        <InfoCard title='Node Metadata'>
+            <StructuredMetadataTable metadata={metadata}/>
+        </InfoCard>
+    );
+}
--- homelab-backstage/plugins/proxmox-frontend/src/pages/NodePage.tsx
+++ homelab-backstage/plugins/proxmox-frontend/src/pages/NodePage.tsx
@@ -3,26 +3,62 @@ import { useEntity } from '@backstage/plugin-catalog-react';
 import { CpuGauge } from '../components/CpuGaugeComponent/CpuGaugeComponent';
 import { MemoryGauge } from '../components/MemoryGaugeComponent/MemoryGaugeComponent';
 import { Grid } from '@material-ui/core';
+import { NodeMetadataCard } from '../components/NodeMetadataCardComponent/NodeMetadataCardComponent';
+import {
+  Content,
+  ContentHeader,
+  InfoCard,
+  Page,
+  SupportButton,
+} from '@backstage/core-components';
+import { VmTable } from '../components/VmTableComponent/VmTableComponent';
 
+export const NodePage: React.FC = () => {
+  const { entity } = useEntity();
 
-export const NodePage = () => {
-    const { entity } = useEntity();
+  const nodeId = entity.metadata.annotations?.['proxmox.io/node-id'];
+  if (!nodeId) {
+    return <div>No Proxmox ID found for this node.</div>;
+  }
 
-    const nodeId = entity.metadata.annotations?.['proxmox.io/node-id'];
-    if (!nodeId) {
-        return <div>No Proxmox ID found for this node.</div>
-    }
+  return (
+    <Page themeId="tool">
+      <Content>
+        <ContentHeader title={`Node: ${nodeId}`}>
+          <SupportButton />
+        </ContentHeader>
 
-    return (
-        <>
-            <Grid container spacing={2}>
-                <Grid item>
-                    <CpuGauge nodeId={nodeId}/>
-                </Grid>
-                <Grid item>
-                    <MemoryGauge nodeId={nodeId}/>
-                </Grid>
+        {/* Main Content */}
+        <Grid container spacing={3}>
+          {/* Left column */}
+          <Grid item xs={12} md={7}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <NodeMetadataCard nodeId={nodeId} />
+              </Grid>
+
+              <Grid item xs={12}>
+                <InfoCard title="Running Workloads">
+                  <VmTable nodeId={nodeId} />
+                </InfoCard>
+              </Grid>
             </Grid>
-        </>
-    );
-}
+          </Grid>
+
+          {/* Right column */}
+          <Grid item xs={12} md={5}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                <CpuGauge nodeId={nodeId} />
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <MemoryGauge nodeId={nodeId} />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Content>
+    </Page>
+  );
+};
