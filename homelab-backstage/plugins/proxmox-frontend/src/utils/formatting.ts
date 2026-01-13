8bf56a3c2aeedda76b386d4916a2c35ea97d7c82
William Mossberg <william.mossberg@outlookc.com>
2026-01-13 18:37:50 +0100

    Started to clean up code.

--- homelab-backstage/plugins/proxmox-frontend/src/utils/formatting.ts
+++ homelab-backstage/plugins/proxmox-frontend/src/utils/formatting.ts
@@ -1,28 +1,35 @@
 export function bytesToGbString(bytes: number): string {
-    const gb = bytes / (1024 * 1024 * 1024);
-    return `${parseFloat(gb.toFixed(1))} GB`;
+  const gb = bytes / (1024 * 1024 * 1024);
+  return `${parseFloat(gb.toFixed(1))} GB`;
 }
 
-
 export function uptimeToString(seconds: number): string {
-    if (seconds <= 0) {
-        return '0m';
-    }
+  if (seconds <= 0) {
+    return '0m';
+  }
 
-    const days = Math.floor(seconds / (3600 * 24));
-    const hours = Math.floor((seconds % (3600 * 24)) / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
+  const days = Math.floor(seconds / (3600 * 24));
+  const hours = Math.floor((seconds % (3600 * 24)) / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
 
-    const uptime = Array<String>();
+  const uptime = Array<String>();
 
-    if (days > 0) {
-        uptime.push(`${days}d`);
-    }
-    if (hours > 0) {
-        uptime.push(`${hours}h`);
-    }
-    
-    uptime.push(`${minutes}m`);
+  if (days > 0) {
+    uptime.push(`${days}d`);
+  }
+  if (hours > 0) {
+    uptime.push(`${hours}h`);
+  }
 
-    return uptime.join(' ');
+  uptime.push(`${minutes}m`);
+
+  return uptime.join(' ');
+}
+
+export function formatTimestamp(unixtime: number): string {
+  return new Date(unixtime * 1000).toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
 }
